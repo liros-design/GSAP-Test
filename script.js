0215b293e2d2046679893d35cde8cf92c76cb92c
liros-design <128178457+liros-design@users.noreply.github.com>
2025-09-19 14:45:52 +0200

    Update script.js
    Added delay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // GSAP animations
-gsap.from(".text-big", { x: -50, opacity: 0, duration: 1, ease: "power2.out" });
-gsap.from(".text-small", { x: -30, opacity: 0, duration: 1, delay: 0.5, ease: "power2.out" });
-gsap.from(".dimedis-logo img", { x: -50, opacity: 0, duration: 3, delay: 1.5, ease: "power2.out" });
+gsap.from(".text-big", { x: -50, opacity: 0, duration: 1, ease: "power2.out", repeat: -1, repeatDelay: 2 });
+gsap.from(".text-small", { x: -30, opacity: 0, duration: 1, delay: 0.5, ease: "power2.out", repeat: -1, repeatDelay: 2 });
+gsap.from(".dimedis-logo img", { x: -50, opacity: 0, duration: 3, delay: 1.5, ease: "power2.out", repeat: -1, repeatDelay: 2 });
