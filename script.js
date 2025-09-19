9d63615683942f4e8a14472423bb3f8822cf2ab5
liros-design <128178457+liros-design@users.noreply.github.com>
2025-09-19 14:52:45 +0200

    Update script.js
    added the newest version from codepen

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-// GSAP animations
-gsap.from(".text-big", { x: -50, opacity: 0, duration: 1, ease: "power2.out", repeat: -1, repeatDelay: 2 });
-gsap.from(".text-small", { x: -30, opacity: 0, duration: 1, delay: 0.5, ease: "power2.out", repeat: -1, repeatDelay: 2 });
-gsap.from(".dimedis-logo img", { x: -50, opacity: 0, duration: 3, delay: 1.5, ease: "power2.out", repeat: -1, repeatDelay: 2 });
+// GSAP Animations //
+gsap.from(".text-big", {
+  x: -50,
+  opacity: 0,
+  duration: 3,
+  ease: "power2.out",
+  repeat: -1,
+  repeatDelay: 2
+});
+
+gsap.from(".text-small", {
+  x: -50,
+  opacity: 0,
+  duration: 3,
+  ease: "power2.out",
+  repeat: -1,
+  repeatDelay: 2
+});
+
+gsap.from(".dimedis-logo img", {
+  x: -50,
+  opacity: 0,
+  duration: 3,
+  delay: 0.5,
+  ease: "power2.out",
+  repeat: -1,
+  repeatDelay: 2
+});
